5ac8feaa2065b898c90344e5393fe079218458ab
fahridgn <fahridgn@gmail.com>
2023-07-26 19:30:42 +0300

    birinci bommit

--- notlarim/models.py
+++ notlarim/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.conf import settings
+import datetime
 
 # Create your models here.
 
 class Notlar(models.Model):
   baslik = models.CharField(max_length=200, verbose_name='Başlık')
   icerik = models.TextField(verbose_name='İçerik')
-  #  date= models.DateTimeField( auto_now_add=True, verbose_name='Tarih', null=True)
-  #  author= models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Yazar')
+  date   = models.DateTimeField(auto_now_add=True, null=True, verbose_name='Tarih')
+  yazar  = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Yazar')
   
 
   def __str__(self):
@@ -23,13 +23,13 @@ class Notlar(models.Model):
 
 
 
-class Comment(models.Model):
-    article=models.ForeignKey(Notlar, verbose_name=("Makale"), on_delete=models.CASCADE, related_name='comments')
-    comment=models.CharField(max_length=160, verbose_name=("Yorum") )
-    author= models.ForeignKey(get_user_model(), verbose_name=("Yazar"), on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.comment
-    
-    def get_absolute_url(self):
-        return reverse('notlari_listele')
+#class Comment(models.Model):
+#    article = models.ForeignKey(Notlar, verbose_name=("Makale"), on_delete=models.CASCADE, related_name='comments')
+#    comment = models.CharField(max_length=160, verbose_name=("Yorum") )
+#    author  = models.ForeignKey(get_user_model(), verbose_name=("Yazar"), on_delete=models.CASCADE)
+#
+#    def __str__(self):
+#        return self.comment
+#    
+#    def get_absolute_url(self):
+#        return reverse('notlari_listele')
--- notlarim/urls.py
+++ notlarim/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
     path('', views.NotlarimView.as_view(), name='notlari_listele'),
-    path('notlarim/icerik/', views.YeniNot.as_view(), name="icerik"),
-    path('notlarim/<int:pk>/', views.NotDetay.as_view(), name='not_detay'),
+    path('not_ekle/', views.YeniNot.as_view(), name="icerik"),
+    path('not_detay/<int:pk>/', views.NotDetay.as_view(), name='not_detay'),
     path('notlarim/<int:pk>/duzenle/', views.NotDuzenle.as_view(), name='not_duzenle'),
     path('notlarim/<int:pk>/uye_sil/', views.NotSil.as_view(), name="not_sil"),
-]
+] 
